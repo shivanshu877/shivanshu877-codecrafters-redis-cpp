443f998d5ee4c78ba8c3189151536c71327e4865
shivanshu877 <vashudev8777@gmail.com>
2024-03-13 19:16:53 +0530

    pass 2st stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -47,11 +47,10 @@ int main(int argc, char **argv) {
   int client_addr_len = sizeof(client_addr);
   
   std::cout << "Waiting for a client to connect...\n";
-  
    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   const char *response ="+PONG\r\n";
-1
+
   send(client_fd, response, strlen(response), 0 );
   
   close(server_fd);
