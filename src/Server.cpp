10b11dd9d5c68b99b20f44a27a182af0a59c0ff6
shivanshu877 <vashudev8777@gmail.com>
2024-03-14 16:23:13 +0530

    4 stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,136 +1,79 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <thread>
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include <sstream>
 #include <cstring>
-#include <vector>
-#include <thread>
-#include <optional>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-
-static constexpr std::string pongStr = "+PONG\r\n";
-static constexpr int MAX_BUFFER = 4096;
-// comment added 
-class Buffer
-{
-public:
-  Buffer()
-  {
-    memset(buffer, 0, sizeof(buffer));
-    readIdx = 0;
-  }
-
-  char *getBuffer() { return buffer; }
-  int size() { return MAX_BUFFER; }
-
-private:
-  char buffer[MAX_BUFFER];
-  int readIdx;
-};
-
-std::optional<Buffer> readSome(int client_fd)
-{
-  Buffer buff;
-
-  int numRecv;
-  if ((numRecv = recv(client_fd, buff.getBuffer(), buff.size(), 0)) < 0)
-  {
-    std::cout << "Error: " << std::strerror(errno) << "\n";
-  }
-  if (numRecv == 0)
-  {
-    return std::nullopt;
-  }
-
-  return buff;
-}
-
-void pingClient(int client_fd)
-{
-  std::optional<Buffer> buffer = std::nullopt;
+#include <pthread.h>
+int handleConnect(int server_connect) {
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
   
-  while ((buffer = readSome(client_fd)) != std::nullopt) {
-    if (buffer == std::nullopt)
-    {
-      break;
-    }
-    if (send(client_fd, pongStr.data(), pongStr.length(), 0) < 0)
-    {
-      std::cout << "Could not ping client\n";
-    }
-  }
+  std::cout << "Waiting for a client to connect...\n";
+  
+  auto clientSocket = accept(server_connect, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  std::cout << "Client connected\n";
+  return clientSocket;
 
-  close(client_fd);
 }
-
-int main(int argc, char **argv)
-{
+void handleClient(int clientSocket) {
+  const char *pong = "+PONG\r\n";
+  char buffer[1024];
+    while(recv(clientSocket, buffer, sizeof(buffer), 0)) {
+      send(clientSocket, pong, strlen(pong), 0);
+    }
+1
+  close(clientSocket);
+}
+int main(int argc, char **argv) {
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
-
-  // Uncomment this block to pass the first stage
+  //Uncomment this block to pass the first stage
+  
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0)
-  {
-    std::cerr << "Failed to create server socket\n";
-    return 1;
+  if (server_fd < 0) {
+   std::cerr << "Failed to create server socket\n";
+   return 1;
   }
-
-  // // Since the tester restarts your program quite often, setting REUSE_PORT
-  // // ensures that we don't run into 'Address already in use' errors
+  
+  // Since the tester restarts your program quite often, setting SO_REUSEADDR
+  // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0)
-  {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-
+  
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-
-  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
-  {
+  
+  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-
+  
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0)
-  {
+  if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
+  
+  bool l = true;
+  while(l) {
+    auto client_connect = handleConnect(server_fd);
+    std::thread thread(&handleClient, client_connect);
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-
-  std::cout << "Waiting for a client to connect...\n";
-
-  std::vector<std::thread> threads;
-  while (true)
-  {
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
-    if (client_fd < 0)
-    {
-      std::cout << "Failed to accept\n";
-      break;
-    }
-
-    threads.emplace_back(pingClient, client_fd);
+    thread.detach();
   }
-
-  for (auto &&thread : threads)
-  {
-    thread.join();
-  }
-
   close(server_fd);
-
   return 0;
 }
